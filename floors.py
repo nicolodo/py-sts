9f20b1ce485ada4c724e976acde9d419a92b3716
Nicolodo <no@example.com>
2025-01-07 20:53:13 +0000

    add floor setup ideas

---
+++ floors.py
@@ -0,0 +1,14 @@
+# floors
+
+# there will be ten floors
+
+# each floor will have 4 rooms
+# each room is a enemy (norm, boss), safe (bonfire, shop) or ? (event, or anything else)
+
+roomTypes = ['enemy','boss','bonfire','shop','?']
+
+floorA = (e,e,e,e)
+
+# along with the floor type it should include the room on the next floor it points to
+
+# (e,1,2)
